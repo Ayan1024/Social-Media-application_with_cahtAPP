e31dd0b1d0d9a72a4836d97437b6a3c1976530e9
Ayan Mukherjee <116819600+Ayan1024@users.noreply.github.com>
2025-09-11 14:22:25 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path"
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
@@ -13,11 +14,10 @@ dotenv.config();
 connectDB()
 // const app = express();
 
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
+
 
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -30,8 +30,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+
 app.use(cookieParser())
 
 //Routes
@@ -39,6 +38,15 @@ app.use(cookieParser())
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/messages", messageRoutes)
+
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
